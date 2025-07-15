2936efffc7f9b4e9583322b9ed7be6126c9fc7ab
Joao Leonardo <joaozacachuka@gmail.com>
2025-07-15 13:14:25 -0300

    Cria componente Navbar básico

---
+++ components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar: React.FC = () => {
+  return (
+    <nav style={{ padding: "1rem", backgroundColor: "#0070f3", color: "white" }}>
+      <h1>CRM Médico</h1>
+    </nav>
+  );
+};
+
+export default Navbar;
